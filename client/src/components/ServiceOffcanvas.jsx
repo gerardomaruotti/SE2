647a0f4ad8bbf8893285909dbeaaa3fb3b6a0921
Gerardo Maruotti <maruotti@icloud.com>
2023-10-25 17:05:43 +0200

    add new service form

--- client/src/components/ServiceOffcanvas.jsx
+++ client/src/components/ServiceOffcanvas.jsx
@@ -1,19 +1,52 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Offcanvas } from 'react-bootstrap';
+import { Offcanvas, Form, Button } from 'react-bootstrap';
+import React, { useState } from 'react';
+import Select from 'react-select';
 
 function ServiceOffcanvas(props) {
 	const show = props.show;
+	const [service, setService] = useState('');
+	const [counters, setCounters] = useState([]);
 	function handleClose() {
 		props.setShow(false);
+		setService('');
+		setCounters([]);
 	}
 
+	function handleSubmit(event) {
+		event.preventDefault();
+		props.setShow(false);
+		setService('');
+		setCounters([]);
+	}
+
+	const counterslist = [
+		{ value: 1, label: 'Counter 1' },
+		{ value: 2, label: 'Counter 2' },
+		{ value: 3, label: 'Counter 3' },
+	];
+
 	return (
-		<Offcanvas show={show} onHide={handleClose} backdrop='static'>
+		<Offcanvas show={show} onHide={handleClose}>
 			<Offcanvas.Header closeButton>
-				<Offcanvas.Title>Offcanvas</Offcanvas.Title>
+				<Offcanvas.Title>Add a new Service</Offcanvas.Title>
 			</Offcanvas.Header>
 			<Offcanvas.Body>
-				Some text as placeholder. In real life you can have the elements you have chosen. Like, text, images, lists, etc.
+				<Form onSubmit={handleSubmit}>
+					<Form.Group className='mb-3' controlId='formServiceName'>
+						<Form.Label>Service Name</Form.Label>
+						<Form.Control type='text' value={service} onChange={(event) => setService(event.target.value)} placeholder='Enter name' />
+					</Form.Group>
+					<Form.Group className='mb-3' controlId='formCounterSelection'>
+						<Form.Label>Select Counters</Form.Label>
+						<Select options={counterslist} isMulti='true' />
+					</Form.Group>
+					<div className='d-grid'>
+						<Button variant='primary' type='submit'>
+							Create Service
+						</Button>
+					</div>
+				</Form>
 			</Offcanvas.Body>
 		</Offcanvas>
 	);
