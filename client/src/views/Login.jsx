0559e88fbac88001f7e5dd07c9de1c043202c24c
Gerardo Maruotti <maruotti@icloud.com>
2023-10-27 09:08:17 +0200

    add back button Login screen

--- client/src/views/Login.jsx
+++ client/src/views/Login.jsx
@@ -29,6 +29,16 @@ function Login(props) {
 	return (
 		<Container>
 			<Row className='justify-content-md-center mt-5'>
+				<Col>
+					<Button onClick={() => navigate('/')}>
+						<svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-arrow-left' viewBox='0 0 16 16'>
+							<path
+								fill-rule='evenodd'
+								d='M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z'
+							/>
+						</svg>
+					</Button>
+				</Col>
 				<Col md={5}>
 					<h1 className='pb-3' style={{ textAlign: 'center' }}>
 						Log in to your account
@@ -63,6 +73,7 @@ function Login(props) {
 						</div>
 					</Form>
 				</Col>
+				<Col></Col>
 			</Row>
 		</Container>
 	);
